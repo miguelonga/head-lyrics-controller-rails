2d107b34cb5d79ea6d001f38616a71b65e8b6ac3
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-05-23 20:38:54 +0200

    fullpage.js added and working

--- js/sample.js
+++ js/sample.js
@@ -1,20 +1,10 @@
 (function(){
-	// consider using a debounce utility if you get too many consecutive events
-	// $(window).on('motion', function(ev, data){
-	// 	console.log('detected motion at', new Date(), 'with data:', data);
-	// 	var spot = $(data.spot.el);
-	// 	spot.addClass('active');
-	// 	setTimeout(function(){
-	// 		spot.removeClass('active');
-	// 	}, 230);
-	// });
 
-	// example using a class
+
 	$('.link').on('motion', function(ev, data){
 		console.log('motion detected on a link to', data.spot.el.href);
 	});
 
-	// examples for id usage
 	$('#one').on('motion', function(){
 		$( 'body' ).css( "background-color", "orange" );
 	});
@@ -22,4 +12,5 @@
 	$('#another').on('motion', function(){
 		$( 'body' ).css( "background-color", "green" );
 	});
+
 })();
