49ddd857a934d3baf59d6772f2c611beef93c5b3
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-05-23 21:22:02 +0200

    text movement depend on webcam motion

--- js/sample.js
+++ js/sample.js
@@ -7,10 +7,14 @@
 
 	$('#one').on('motion', function(){
 		$( 'body' ).css( "background-color", "orange" );
+		$.fn.fullpage.moveSectionUp();
 	});
 
 	$('#another').on('motion', function(){
+
 		$( 'body' ).css( "background-color", "green" );
+		$.fn.fullpage.moveSectionDown();
+
 	});
 
 })();
