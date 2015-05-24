822c692cb5e26b866da99c68299cc04b10f6ee92
Mercedes Egido <meegido@gmail.com>
2015-05-24 11:38:43 +0200

    initialization scroll in js document

--- js/sample.js
+++ js/sample.js
@@ -18,3 +18,10 @@
 	});
 
 })();
+
+ $(document).ready(function() {
+        $('#fullpage').fullpage({
+            anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage', '7thPage'],
+            scrollBar: true
+        });
+    });
