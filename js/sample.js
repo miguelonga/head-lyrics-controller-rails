8b4b289ae44c9139f1eca9474990f969bab0f105
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-05-25 11:30:27 +0200

    changing to master

--- js/sample.js
+++ js/sample.js
@@ -1,7 +1,6 @@
 (function(){
 	var counter = 0;
-
-
+	var counter2 = 0;
 
 	$('.link').on('motion', function(ev, data){
 		console.log('motion detected on a link to', data.spot.el.href);
@@ -10,11 +9,12 @@
 	$('#one').on('motion', function(){
 		console.log('izquierda---------')
 		$( 'body' ).css( "background-color", "orange" );
-		counter++;
-		if (counter == 5) {
+		counter2++;
+		if (counter2 == 9) {
 			$.fn.fullpage.moveSectionUp()
-			counter = 0;
+			counter2 = 0;
 		};
+		document.querySelector('.counter2').innerHTML = counter2;
 		
 	});
 
@@ -23,11 +23,15 @@
 		$( 'body' ).css( "background-color", "green" );
 		counter++;
 		console.log(counter);
-		if (counter == 5) {
+		if (counter == 9) {
 			$.fn.fullpage.moveSectionDown();
 			counter = 0;
 		};
+		document.querySelector('.counter').innerHTML = counter;
+
 	});
+
+
 
 
 
