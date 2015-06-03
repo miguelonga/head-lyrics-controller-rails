7575ab57aafe124f1ee090e9a20b31c37cdcf456
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-06-03 22:57:29 +0200

    start button takes you to the game with the lyrics of the new song created

--- js/our_parse.js
+++ js/our_parse.js
@@ -8,54 +8,61 @@ $(function(){
         mySong.set('name', $('#song_name').val());
         mySong.set('sections', $('#song').val().split('..'))
         mySong.save().then(function(object) {
-          console.log($('#song_name').val());
-          console.log($('#song').val());
         });
 
      
     });
 
-    function getSections(){
-        //Once again, we extend the Parse.Object class to make the ListItem class
-        Song = Parse.Object.extend("Song");
+    getFavorites();
 
-        //This time, we use Parse.Query to generate a new query, specifically querying the ListItem table.
-        query = new Parse.Query(Song);
-
-        //We set constraints on the query.
-        query.get("mLx7xRmSvg", {
-          success: function(song) {
-            // The object was retrieved successfully.
-            mySections = song.get('sections');
-            console.log(mySections);
-
-            for (i = 0; i < mySections.length; i++) { 
-                document.getElementById("section" + i).innerHTML = mySections[i];
+    function getFavorites(){
+        var Song = Parse.Object.extend("Song");
+        var query = new Parse.Query(Song);
+        query.limit(5);
+        query.find({
+          success: function(results) {
+            for (var i = 0; i < results.length; i++) { 
+              var song = results[i];
+              console.log(results[i].id);
+              $('#item' + i).innerHTML = song.id;
             }
-
-            // document.getElementById("section0").innerHTML = mySections[0];
-            // document.getElementById("section1").innerHTML = mySections[1];
-            // document.getElementById("section2").innerHTML = mySections[2];
-            // document.getElementById("section3").innerHTML = mySections[3];
-            // document.getElementById("section4").innerHTML = mySections[4];
-            // document.getElementById("section5").innerHTML = mySections[5];
-            // document.getElementById("section6").innerHTML = mySections[6];
-            // document.getElementById("section7").innerHTML = mySections[7];
-            // document.getElementById("section8").innerHTML = mySections[8];
-            // document.getElementById("section9").innerHTML = mySections[9];
-            // document.getElementById("section10").innerHTML = mySections[10];
-
           },
-          error: function(object, error) {
-            // The object was not retrieved successfully.
-            // error is a Parse.Error with an error code and message.
+          error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
           }
         });
-        var sections = query.get("sections");
-        console.log(query);
-
     }
 
-    getSections();
+
+    $('#search_song').click(function(event){
+        event.preventDefault();
+        Song = Parse.Object.extend("Song");
+        query = new Parse.Query(Song);
+        mySong = query.equalTo("name", $('#searcher').val());
+        query.find({
+          success: function(mySong) {
+            getSections(mySong[0].id);
+          },
+          error: function(error) {
+            console.log('error en la busqueda')
+          }
+        });
+
+        });
+
+     
+    function getSections(id){
+        Song = Parse.Object.extend("Song");
+        query = new Parse.Query(Song);
+        query.get(id, {
+          success: function(song) {
+            mySections = song.get('sections');
+          },
+          error: function(object, error) {
+            console.log('error en getSections');
+          }
+        });
+
+    }
 
 }); 
