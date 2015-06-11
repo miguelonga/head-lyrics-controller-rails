d4921523bca10f671bf59b55ab2040af1944b89a
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-06-11 23:46:39 +0200

    added uniqueness user tests validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611210929) do
+ActiveRecord::Schema.define(version: 20150611213949) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20150611210929) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
