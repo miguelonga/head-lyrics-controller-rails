51def7f97e98e4d914584204903763ef297699bd
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-06-11 23:37:08 +0200

    user model and user_tests added

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 40 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+end
