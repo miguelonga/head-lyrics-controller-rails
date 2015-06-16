0b16df7e88a3dc738a1cea58969c329266ad8093
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-06-16 13:26:26 +0200

    correct_user method for edit and update actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,11 @@ module SessionsHelper
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
+	# Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
 	def logged_in?
   	!current_user.nil?
 	end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
@@ -22,4 +23,18 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
 end
