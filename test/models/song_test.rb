6c9c95d4e6fae84273658185e8dc315c0d788edf
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-06-16 17:52:59 +0200

    second travis try with song tests

--- test/models/song_test.rb
+++ test/models/song_test.rb
@@ -1,5 +1,32 @@
 require 'test_helper'
 
 class SongTest < ActiveSupport::TestCase
-  
+  def setup
+    @user = users(:michael)
+    @song = @user.songs.build(name: "Foo Bar", content: "a" * 241)
+  end
+
+  test "should be valid" do
+    assert @song.valid?
+  end
+
+  test "user id should be present" do
+    @song.user_id = nil
+    assert_not @song.valid?
+  end
+
+  test "name should be present" do
+    @song.name = "   "
+    assert_not @song.valid?
+  end
+
+  test "content should be present" do
+    @song.content = "   "
+    assert_not @song.valid?
+  end
+
+  test "content should be at less 240 characters" do
+    @song.content = "Ñorem impsumun"
+    assert_not @song.valid?
+  end
 end
